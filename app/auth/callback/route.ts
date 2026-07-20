2173277aac88d73faa3b6f6c54dc69cd6caa738a
Sebastian Palacio Hernandez <sebastianpalaciohernandez8@gmail.com>
2026-07-19 20:49:06 -0500

    fix: :bug: A login bug redirects me to a page that doesn't exist

--- app/auth/callback/route.ts
+++ app/auth/callback/route.ts
@@ -6,7 +6,7 @@ import type { NextRequest } from 'next/server'
 export async function GET(request: NextRequest) {
   const { searchParams, origin } = new URL(request.url)
   const code = searchParams.get('code')
-  const next = searchParams.get('next') ?? '/test-business'
+  const next = searchParams.get('next') ?? '/dashboard'
 
   if (code) {
     const supabase = await createClient()
